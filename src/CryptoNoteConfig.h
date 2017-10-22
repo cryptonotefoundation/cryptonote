4f1497ca232725534b13d360b30bcfe4e5f87187
Adrian Johnson <adrian@cqdev.co.uk>
2017-10-22 11:52:56 +0100

    added genesis thingy

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "im-gonna-fucking-kill-you-mum-coin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char GENESIS_COINBASE_TX_HEX[] = "013c01ff000103029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101ce15a61f967fd1cdbc10180f6086ac6b69ac99024af4b8921ff1518d95a7f1db";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
