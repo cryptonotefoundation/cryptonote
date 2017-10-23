ed742c3bff5b9a5fcea573f615831ab22a44f4a6
DOPEBOI <32817772+DOPEBOI@users.noreply.github.com>
2017-10-24 00:50:41 +0200

    Update Daemon.cpp

--- src/Daemon/Daemon.cpp
+++ src/Daemon/Daemon.cpp
@@ -46,12 +46,12 @@ namespace
   const command_line::arg_descriptor<bool>        arg_console     = {"no-console", "Disable daemon console commands"};
   const command_line::arg_descriptor<bool>        arg_testnet_on  = {"testnet", "Used to deploy test nets. Checkpoints and hardcoded seeds are ignored, "
     "network id is changed. Use it with --data-dir flag. The wallet must be launched with --testnet flag.", false};
-  const command_line::arg_descriptor<bool>        arg_print_genesis_tx = { "Dopesd --print-genesis-tx", "Prints genesis' block tx hex to insert it to config and exits" };
+  const command_line::arg_descriptor<bool>        arg_print_genesis_tx = { "print-genesis-tx", "Prints genesis' block tx hex to insert it to config and exits" };
 }
 
 bool command_line_preprocessor(const boost::program_options::variables_map& vm, LoggerRef& logger);
 
-void print_genesis_tx_hex() {
+void print_genesis_tx_hex(Dopesd --print-genesis-tx) {
   Logging::ConsoleLogger logger;
   CryptoNote::Transaction tx = CryptoNote::CurrencyBuilder(logger).generateGenesisTransaction();
   CryptoNote::BinaryArray txb = CryptoNote::toBinaryArray(tx);
