2784dc65f34fe953e2a5657fc83b5fbd9307e2ab
BLockChainsB <61760974+BLockChainsB@users.noreply.github.com>
2020-03-10 19:48:44 -0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -40,7 +40,7 @@ static_assert(EMISSION_SPEED_FACTOR <= 25 * sizeof(uint64_t), "EMISSION_SPEED_FA
 
 //TODO Define number of blocks for block size median calculation
 const size_t   BITCUTY_REWARD_BLOCKS_WINDOW                  = 15; // !!!
-const size_t   BITCUTY_BLOCK_GRANTED_FULL_REWARD_ZONE        = 783333; // Magic-bit le bit MB, swap abracadabra octet par bit en devise, est la blockchain cryptonote Bitcuty !!!
+const size_t   BITCUTY_BLOCK_GRANTED_FULL_REWARD_ZONE        = 783333; // Magic-bit the MB bit, abracadabra byte per bit swap to currency, is the Cryptonote Blockchain Bitcuty !!!
 const size_t   BITCUTY_COINBASE_BLOB_RESERVED_SIZE           = 94371; // Equal nine percent of block space !!!
 //TODO Define number of digits
 const size_t   BITCUTY_DISPLAY_DECIMAL_POINT                 = 12; // It's BB-BCUTY !!!
@@ -53,74 +53,74 @@ const uint64_t DIFFICULTY_TARGET                             = 15; // The data i
 const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 * 1000 * 1 / DIFFICULTY_TARGET; // "'%H:%M:%S:%MS / Difficulties Targeting', '15: 0\n0 in 15 sec'"
 //TODO There are options to tune CryptoNote's difficulty retargeting function.
 //TODO We recommend not to change it.
-const size_t   DIFFICULTY_WINDOW                             = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;
-const size_t   DIFFICULTY_CUT                                = 5759998;
-const size_t   DIFFICULTY_LAG                                = 15; // !!!
-static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_WINDOW - 2, "DIFFICULTY_WINDOW or DIFFICULTY_CUT");
+const size_t   DIFFICULTY_WINDOW                             = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY; // Bitcuty is Bitcuty !!!
+const size_t   DIFFICULTY_CUT                                = 5759998; // (-)_(*) mining all`in !!!
+const size_t   DIFFICULTY_LAG                                = 0; // selfajustment to best performances !!!
+static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_WINDOW - 2, "DIFFICULTY_WINDOW or DIFFICULTY_CUT"); // equals 0.end !!!
 
-const size_t   MAX_BLOCK_SIZE_INITIAL                        = 1024 * 1024;
-const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_NUMERATOR         = 4096 * 1024;
-const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_DENOMINATOR       = 365 * 24 * 60 * 60 / DIFFICULTY_TARGET;
+const size_t   MAX_BLOCK_SIZE_INITIAL                        = 1024 * 1024; // 1MB  (in binary)!!!
+const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_NUMERATOR         = 4096 * 1024; // 4 MB (in binary) !!!
+const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_DENOMINATOR       = 365 * 24 * 60 * 60 / DIFFICULTY_TARGET; // Works all the time !!!
 
-const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS     = 1;
-const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS    = DIFFICULTY_TARGET * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS;
+const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS     = 1; // !!!
+const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS    = DIFFICULTY_TARGET * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS; // !!!
 
-const uint64_t CRYPTONOTE_MEMPOOL_TX_LIVETIME                = 60 * 60 * 24;    
-const uint64_t CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME = 60 * 60 * 24 * 7; 
-const uint64_t CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL = 7; 
+const uint64_t CRYPTONOTE_MEMPOOL_TX_LIVETIME                = 60 * 60 * 24; // !!!    
+const uint64_t CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME = 60 * 60 * 24 * 7; // !!! 
+const uint64_t CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL = 7; // !!! 
   
-const size_t   FUSION_TX_MAX_SIZE                            = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE * 30 / 100;
-const size_t   FUSION_TX_MIN_INPUT_COUNT                     = 12;
-const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
+const size_t   FUSION_TX_MAX_SIZE                            = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE * 30 / 100; // !!!
+const size_t   FUSION_TX_MIN_INPUT_COUNT                     = 12; // !!!
+const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4; // !!!
 
-const char     CRYPTONOTE_BLOCKS_FILENAME[]                  = "blocks.dat";
-const char     CRYPTONOTE_BLOCKINDEXES_FILENAME[]            = "blockindexes.dat";
-const char     CRYPTONOTE_BLOCKSCACHE_FILENAME[]             = "blockscache.dat";
-const char     CRYPTONOTE_POOLDATA_FILENAME[]                = "poolstate.bin";
-const char     P2P_NET_DATA_FILENAME[]                       = "p2pstate.bin";
-const char     CRYPTONOTE_BLOCKCHAIN_INDICES_FILENAME[]      = "blockchainindices.dat";
-const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json";
+const char     CRYPTONOTE_BLOCKS_FILENAME[]                  = "blocks.dat"; // !!!
+const char     CRYPTONOTE_BLOCKINDEXES_FILENAME[]            = "blockindexes.dat"; // !!!
+const char     CRYPTONOTE_BLOCKSCACHE_FILENAME[]             = "blockscache.dat"; // !!!
+const char     CRYPTONOTE_POOLDATA_FILENAME[]                = "poolstate.bin"; // !!!
+const char     P2P_NET_DATA_FILENAME[]                       = "p2pstate.bin"; // !!!
+const char     CRYPTONOTE_BLOCKCHAIN_INDICES_FILENAME[]      = "blockchainindices.dat"; // !!!
+const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"; // Bitcuty is the best !!!
 } // parameters
 
 //TODO Put here the name of your currency
-const char     CRYPTONOTE_NAME[]                             = "Bitcuty";
-const char     CRYPTONOTE_SYMBOL[]                           = "BCUTY";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     CRYPTONOTE_NAME[]                             = "Bitcuty"; // BCUTY !!!
+const char     CRYPTONOTE_SYMBOL[]                           = "BCUTY"; // Bitcuty !!!
+const char     GENESIS_COINBASE_TX_HEX[]                     = ""; // The best first block to the first blob chain and the only first !!!
 
-const uint8_t  TRANSACTION_VERSION_1                         =  1;
-const uint8_t  TRANSACTION_VERSION_2                         =  2;
-const uint8_t  CURRENT_TRANSACTION_VERSION                   =  TRANSACTION_VERSION_1;
-const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
-const uint8_t  BLOCK_MAJOR_VERSION_2                         =  2;
-const uint8_t  BLOCK_MAJOR_VERSION_3                         =  3;
-const uint8_t  BLOCK_MINOR_VERSION_0                         =  0;
-const uint8_t  BLOCK_MINOR_VERSION_1                         =  1;
+const uint8_t  TRANSACTION_VERSION_1                         =  1; // !!!
+const uint8_t  TRANSACTION_VERSION_2                         =  2; // !!!
+const uint8_t  CURRENT_TRANSACTION_VERSION                   =  TRANSACTION_VERSION_1; // !!!
+const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1; // !!!
+const uint8_t  BLOCK_MAJOR_VERSION_2                         =  2; // !!!
+const uint8_t  BLOCK_MAJOR_VERSION_3                         =  3; // !!!
+const uint8_t  BLOCK_MINOR_VERSION_0                         =  0; // !!!
+const uint8_t  BLOCK_MINOR_VERSION_1                         =  1; // !!!
 
-const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by default, blocks ids count in synchronizing
-const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  100;    //by default, blocks count in blocks downloading
-const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
+const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  // by default, blocks ids count in synchronizing nows !!!
+const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  100;    // by default, blocks count in blocks downloading nows !!!
+const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;   // !!!
 //TODO This port will be used by the daemon to establish connections with p2p network
-const int      P2P_DEFAULT_PORT                              = 3984;
+const int      P2P_DEFAULT_PORT                              = 3984; // Hashing-b and Cryptonote, it`s solid !!!
 //TODO This port will be used by the daemon to interact with simlewallet
-const int      RPC_DEFAULT_PORT                              = 445;
+const int      RPC_DEFAULT_PORT                              = 445; // Hashing-b and Cryptonote, are solid !!!
   
-const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
-const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
+const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000; // !!!
+const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000; // !!!
 
-const size_t   P2P_CONNECTION_MAX_WRITE_BUFFER_SIZE          = 16 * 1024 * 1024; // 16 MB
-const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT                 = 8;
-const size_t   P2P_DEFAULT_WHITELIST_CONNECTIONS_PERCENT     = 70;
+const size_t   P2P_CONNECTION_MAX_WRITE_BUFFER_SIZE          = 16 * 1024 * 1024; // 16 MB !!!
+const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT                 = 8; // !!!
+const size_t   P2P_DEFAULT_WHITELIST_CONNECTIONS_PERCENT     = 70; // !!!
 const uint32_t P2P_DEFAULT_HANDSHAKE_INTERVAL                = 60;            // seconds
 const uint32_t P2P_DEFAULT_PACKET_MAX_SIZE                   = 50000000;      // 50000000 bytes maximum packet size
-const uint32_t P2P_DEFAULT_PEERS_IN_HANDSHAKE                = 250;
+const uint32_t P2P_DEFAULT_PEERS_IN_HANDSHAKE                = 250; // !!!
 const uint32_t P2P_DEFAULT_CONNECTION_TIMEOUT                = 5000;          // 5 seconds
 const uint32_t P2P_DEFAULT_PING_CONNECTION_TIMEOUT           = 2000;          // 2 seconds
 const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; // 2 minutes
 const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
-const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
+const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115"; // !!!
 
 
-//TODO Add here your network seed nodes (tag BCUTY or BCUTY tag)
+//TODO Add here your network seed nodes (tag BCUTY or BCUTY tag), Bitcuty on global accessibility !!!
 const std::initializer_list<const char*> SEED_NODES = {
 // Localhost Bitcuty
 "127.0.0.1:3984"
@@ -144,7 +144,7 @@ const std::initializer_list<const char*> SEED_NODES = {
 "45.32.50.38:3984",
 };
 
-// Bitcuty UUID daemon losthost "127.0.0.1", Briefly All`in
+// Bitcuty UUID daemon losthost "127.0.0.1", Briefly All`in !
 constexpr UUID_BITCUTY_NETWORK = common :: pfh <UUID> ("21d0cb49-1b49-4756-861e-a4deab7a36ab"); // Everything is for the better
 
 struct CheckpointData {
@@ -161,6 +161,6 @@ __attribute__((unused))
 const std::initializer_list<CheckpointData> CHECKPOINTS = {
   //{ 10000, "84b6345731e2702cdaadc6ce5e5238c4ca5ecf48e3447136b2ed829b8a95f3ad" },
 };
-} // CryptoNote
+} // CryptoNote !!!
 
 #define ALLOW_DEBUG_COMMANDS
